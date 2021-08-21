9b255cff4db6ad67e0f3035d6b427a348bd45e7a
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-08-21 03:40:20 -0300

    Pagina que mostra todos os produtos a venda feito com sucesso

--- src/components/compras/compras.jsx
+++ src/components/compras/compras.jsx
@@ -1,71 +1,49 @@
-import React from 'react'
-import CardCompra from '../layout/cardCompra'
-import trufa from '../../assets/imgs/produtos/trufas/trufa-de-maracuja.jpg'
-import './compras.css'
+import React, { useMemo, useState } from "react"
+import CardCompra from "../layout/cardCompra"
+import "./compras.css"
+import produtos from '../../data/produtos'
 
-export default function Compras(props){
-    return(
-        <main className='compras'>
-            <div className='filtros-ordem'>
-                <form> 
-                    <select name="" id="">
-                        <option value="">Ordem crescende</option>
-                        <option value="">Ordem decrescende</option>
-                        <option value="">Maior valor</option>
-                        <option value="">Menor valor</option>
-                    </select>
-                </form>
-            </div>
-            <div className='vitrine'>
-                <div className='filtro-tipo'>
-                    <p>Tipos</p>
-                    <ul>
-                        <a href='./'>
-                            <li className='cone'>Cones</li>
-                        </a>
+export default function Compras (props){
+  const [busca, setBusca] = useState('')
 
-                        <a href='./'>
-                            <li className='bolo'>Bolo</li>
-                        </a>
-                        
-                        <a href='./'>
-                            <li className='trufa'>Trufa</li>
-                        </a>
-                        <a href='./'>
-                            <li className='mousses'>Mousse</li>
-                        </a>
-                    </ul>
-                </div>
-                <div className='produtos'>
-                    <CardCompra
-                        link='./' 
-                        imagem={trufa} 
-                        titulo='Trunfa' 
-                        descricao='Serei o que quiser. Mas tenho que querer o que for.'
-                        preco='4,00'/>
-                    <CardCompra
-                        link='./' 
-                        imagem={trufa} 
-                        titulo='Trunfa' 
-                        descricao='Serei o que quiser. Mas tenho que querer o que for.'/>
-                    <CardCompra
-                        link='./' 
-                        imagem={trufa} 
-                        titulo='Trunfa' 
-                        descricao='Serei o que quiser. Mas tenho que querer o que for.'/>
-                    <CardCompra
-                        link='./' 
-                        imagem={trufa} 
-                        titulo='Trunfa' 
-                        descricao='Serei o que quiser. Mas tenho que querer o que for.'/>
-                    <CardCompra
-                        link='./' 
-                        imagem={trufa} 
-                        titulo='Trunfa' 
-                        descricao='Serei o que quiser. Mas tenho que querer o que for.'/>
-                    
-                </div>
-            </div>
-        </main>
+  const produtosFiltrados = useMemo (() =>{
+    const lowerBusca = busca.toLowerCase()
+    return produtos.filter((produto) => 
+      produto.nome.toLowerCase().startsWith(lowerBusca)
     )
+  }, [busca])
+  
+  return (
+    <main className="compras">
+      <div className="filtros-ordem">
+        <label>
+          Busca: 
+          <input 
+            type='text'
+            value={busca}
+            onChange={ (evento) => setBusca(evento.target.value) }/>
+        </label>
+        <select name="" id="">
+          <option value="">Maior valor</option>
+          <option value="">Menor valor</option>
+        </select>
+      </div>
+      <div className="vitrine">
+        <div id='produtos' className="produtos">
+          {
+            produtosFiltrados.map((pr) => 
+              <CardCompra
+                key={pr.id}
+                link={pr.link}
+                imagem={"../../" + pr.imagem}
+                titulo={pr.nome}
+                descricao={pr.descricao}
+                preco={pr.preco}
+              />
+            )
+          }
+        </div>
+      </div>
+    </main>
+  )
 }
---
+++ src/data/produtos.js
@@ -0,0 +1,150 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    { 
+      "id": 1,
+      "nome": "Cone de Chocolate",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Chocolate",
+      "tipo": "Cone",
+      "imagem": "assets/imgs/produtos/cones/cone-de-chocolate.jpg",
+      "preco": "4,00"
+    },
+    {
+      "id": 2,
+      "nome": "Cone de Morango",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Morango",
+      "tipo": "Cone",
+      "imagem": "assets/imgs/produtos/cones/cone-de-morango.jpg",
+      "preco": "4,00"
+    },
+    {
+      "id": 3,
+      "nome": "Cone de Maracuja",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Maracuja",
+      "tipo": "Cone",
+      "imagem": "assets/imgs/produtos/cones/cone-de-maracuja.jpg",
+      "preco": "4,00"
+    },
+    {
+      "id": 4,
+      "nome": "Cone de Limão",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Limão",
+      "tipo": "Cone",
+      "imagem": "assets/imgs/produtos/cones/cone-de-limao.png",
+      "preco": "4,00"
+    },
+
+    {
+      "id": 5,
+      "nome": "Trufa de Morango",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Morango",
+      "tipo": "Trufa",
+      "imagem": "assets/imgs/produtos/trufas/trufa-de-morango.jpg",
+      "preco": "5,00"
+    },
+    {
+      "id": 6,
+      "nome": "Trufa de Maracuja",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Maracuja",
+      "tipo": "Trufa",
+      "imagem": "assets/imgs/produtos/trufas/trufa-de-maracuja.jpg",
+      "preco": "5,00"
+    },
+    {
+      "id": 7,
+      "nome": "Trufa de Nutella",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Nutella",
+      "tipo": "Trufa",
+      "imagem": "assets/imgs/produtos/trufas/trufa-de-nutella.jpg",
+      "preco": "5,00"
+    },
+    {
+      "id": 8,
+      "nome": "Trufa de Chocolate",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Chocolate",
+      "tipo": "Trufa",
+      "imagem": "assets/imgs/produtos/trufas/trufa-de-chocolate.jpg",
+      "preco": "5,00"
+    },
+
+    {
+      "id": 9,
+      "nome": "Mousse de Chocolate",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Chocolate",
+      "tipo": "Mousse",
+      "imagem": "assets/imgs/produtos/mousses/mousse-de-chocolate.jpg",
+      "preco": "9,00"
+    },
+    {
+      "id": 10,
+      "nome": "Mousse de Limão",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Limão",
+      "tipo": "Mousse",
+      "imagem": "assets/imgs/produtos/mousses/mousse-de-limao.jpg",
+      "preco": "9,00"
+    },
+    {
+      "id": 11,
+      "nome": "Mousse de Maracuja",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Maracuja",
+      "tipo": "Mousse",
+      "imagem": "assets/imgs/produtos/mousses/mousse-de-maracuja.jpg",
+      "preco": "9,00"
+    },
+    {
+      "id": 12,
+      "nome": "Mousse de Morango",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Morango",
+      "tipo": "Mousse",
+      "imagem": "assets/imgs/produtos/mousses/mousse-de-morango.jpg",
+      "preco": "9,00"
+    },
+
+    {
+      "id": 13,
+      "nome": "Bolo de Cenoura com Brigadeiro",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Cenoura com Brigadeiro",
+      "tipo": "Bolo",
+      "imagem": "assets/imgs/produtos/bolos/bolo-de-cenoura-com-brigadeiro.jpg",
+      "preco": "30,00"
+    },
+    {
+      "id": 14,
+      "nome": "Bolo de Leite Ninho",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Leite Ninho",
+      "tipo": "Bolo",
+      "imagem": "assets/imgs/produtos/bolos/bolo-de-leite-ninho.jpg",
+      "preco": "30,00"
+    },
+    {
+      "id": 15,
+      "nome": "Bolo de Chocolate",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Chocolate",
+      "tipo": "Bolo",
+      "imagem": "assets/imgs/produtos/bolos/bolo-de-chocolate.jpg",
+      "preco": "30,00"
+    },
+    {
+      "id": 16,
+      "nome": "Bolo de Morango",
+      "descricao": "Cone blabla recheado com blabla e muse de blabla",
+      "sabor": "Morango",
+      "tipo": "Bolo",
+      "imagem": "assets/imgs/produtos/bolos/bolo-de-morango.jpg",
+      "preco": "30,00"
+    }
+  ]
